64d52228dced05bd8b0a40c411992c9d192ede77
TuranKh <turan.khidilov@gmail.com>
2021-11-27 11:07:42 +0400

    Chinici.az

---
+++ script.js
@@ -0,0 +1,110 @@
+function changeBasketImage(element) {
+    var image = (element.childNodes[1]);
+    if (image.src == "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/shop-icon.svg")
+    {
+        image.src = "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/shop-icon-hover.svg"
+    }
+}
+function returnBasketImage(element) {
+    var image = (element.childNodes[1]);
+    if (image.src == "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/shop-icon-hover.svg")
+    {
+        image.src = "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/shop-icon.svg"
+    }
+}
+function changeProfileImage(element) {
+    var image = (element.childNodes[1]);
+    if (image.src == "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/profile-icon.svg")
+    {
+        image.src = "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/profile-icon-hover.svg"
+    }
+}
+function returnProfileImage(element) {
+    var image = (element.childNodes[1]);
+    if (image.src == "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/profile-icon-hover.svg")
+    {
+        image.src = "file:///D:/Desktop/Projects/Cinici.az/Pictures/Icons/profile-icon.svg"
+    }
+}
+
+
+function itemFullScreen() {
+    (document.getElementsByClassName("main-section")[0]).style.display = "none"
+    document.getElementById("product-full-screen").style.display = "block"
+}   
+function minimiseFullScreen(){
+    
+    (document.getElementsByClassName("main-section")[0]).style.display = "block"
+    document.getElementById("product-full-screen").style.display = "none"
+}
+
+/*
+    Boyuk 500px;
+    Umumi scroll 2210px;
+    header: 170px;
+    ara mesafesi: 10px;
+
+    510px bir sekil.
+    currentScrollPosition;
+    (800 - 170)/510 
+*/
+
+function moveOnScroll() {
+    window.addEventListener('scroll', () => {
+        let y = window.scrollY
+        let distance = document.getElementsByClassName('secondary-product-image')
+
+        let finalValue = 170 - y*0.12;
+        if(finalValue > -1100) {
+            distance[0].style.top = `${finalValue}px`
+
+        }
+        let previousItem=0;
+        
+        if (-13<finalValue < 170 && finalValue > -13) {
+            document.getElementsByClassName("item-left")[previousItem].style.opacity = 1
+            document.getElementsByClassName("item-left")[previousItem + 1].style.opacity = 0.7;
+        }
+
+        else if (finalValue < -13 && finalValue > -198) {
+            controlActiveElement(0);
+        }
+        else if (finalValue < -198 && finalValue > -382) {
+            controlActiveElement(1);
+        }
+        else if (finalValue < -382 && finalValue > -566) {
+            controlActiveElement(2);
+
+        }
+        else if (finalValue < -566 && finalValue > -750) {
+            controlActiveElement(3);
+
+        }
+        else if (finalValue < -750 && finalValue > -934) {
+            controlActiveElement(4);
+        }
+        
+        else if (finalValue < -934 && finalValue > -1118) {
+            controlActiveElement(5);
+        }
+
+        else if (finalValue < -1118 && finalValue > -1302) {
+            controlActiveElement(6);
+        }
+
+        else if (finalValue < -1302 && finalValue > -1486) {
+            controlActiveElement(7);
+        }
+        
+        else if (finalValue < -1486 && finalValue > -1670) {
+            controlActiveElement(8);
+        }
+    
+    })
+}
+
+function controlActiveElement (previousItem) {
+    document.getElementsByClassName("item-left")[previousItem].style.opacity = 0.7;
+    document.getElementsByClassName("item-left")[previousItem + 1].style.opacity = 1;
+    document.getElementsByClassName("item-left")[previousItem + 2].style.opacity = 0.7;
+}
